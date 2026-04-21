cd4a7b53e7ead518c606fe9a97a1e77ca57ee9e8
Vince <vinceashlee16@gmail.com>
2026-04-21 21:57:15 +0800

    idk mane

--- ebin-backend/server.js
+++ ebin-backend/server.js
@@ -22,7 +22,7 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
 }));
-app.options('*', cors());
+app.options(/.*/, cors());
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
